d3741d312d413c6160c11776072c8a69ef3a663d
DreamerVulpi <dreamervulpi@gmail.com>
2021-12-07 18:26:30 +0300

    fixed fixed.php

--- fixed.php
+++ fixed.php
@@ -1,5 +1,6 @@
 <?php
 
+
 if( isset( $_GET[ 'Login' ] )) {
 	// Get username
 	$user = preg_grep("[A-z]", Array($_GET['username']))[0];
@@ -9,6 +10,7 @@ if( isset( $_GET[ 'Login' ] )) {
 	// Check the database
 	$query  = "SELECT * FROM `users` WHERE user = '$user' AND password = '$pass';";
 	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+	$fp = fopen("file.txt", "a+")
 	if( $result && mysqli_num_rows( $result ) == 1 ) {
 		// Get users details
 		$row    = mysqli_fetch_assoc( $result );
@@ -17,10 +19,16 @@ if( isset( $_GET[ 'Login' ] )) {
 		$html .= "<p>Welcome to the password protected area {$user}</p>";
 		$html .= "<img src=\"{$avatar}\" />";
 	} else {
-		// Login failed
-		$html .= "<pre><br />Username and/or password incorrect.</pre>";
-		sleep(600)
+		if(strpos(file_get_contents("file.txt"), $user)) {
+			// Login failed
+			$html .= "<pre><br />You are blocked. Send message to admin for fix problem: admin@example.com</pre>";
+		} else {
+			$html .= "<pre><br />Username and/or password incorrect.</pre>";
+			sleep(rand(100, 600));
+			fwrite($fp, $file . PHP_EOL)
+		}
 	}
+	fclose(fp);ы
 	((is_null($___mysqli_res = mysqli_close($GLOBALS["___mysqli_ston"]))) ? false : $___mysqli_res);
 }
 
